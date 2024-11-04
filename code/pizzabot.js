9bb16293c0aadc40a3ca578df9c88179a260f4d0
Zarha <zarhaselene@gmail.com>
2024-11-04 12:35:34 +0100

    Add pizza ordering functionality with customer greeting and order total calculation. Task: 1.2-2.3-2.4

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -5,3 +5,20 @@ const pepperoni = "Pepperoni Pizza";
 const pizzaPrice = 80;
 
 //Put your Javscript code here:
+
+// 1.2 Greeting the customer
+alert(
+  `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`
+);
+
+// 2.3 Ask the user which pizza they want, and how many
+const orderName = prompt(
+  `Enter the name of the pizza you want to order today.`
+);
+const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+
+// 2.4 Finalizing the order
+const orderTotal = orderQuantity * pizzaPrice;
+alert(
+  `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`
+);
