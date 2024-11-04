62d13c5d54b4e5ec0c4245672ede9d74b2ac68d1
Zarha <zarhaselene@gmail.com>
2024-11-04 13:30:14 +0100

    Conditionals: Enhance pizza ordering process with validation and cooking time estimation

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -14,11 +14,27 @@ alert(
 // 2.3 Ask the user which pizza they want, and how many
 const orderName = prompt(
   `Enter the name of the pizza you want to order today.`
-);
-const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+).toLowerCase();
 
-// 2.4 Finalizing the order
-const orderTotal = orderQuantity * pizzaPrice;
-alert(
-  `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`
-);
+if (
+  orderName === vegetarian.toLowerCase() ||
+  orderName === hawaiian.toLowerCase() ||
+  orderName === pepperoni.toLowerCase()
+) {
+  // 2.4 Finalizing the order
+  const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+  const orderTotal = orderQuantity * pizzaPrice;
+  let cookingTime = 0;
+  if (orderQuantity >= 1 && orderQuantity <= 2) {
+    cookingTime = 10;
+  } else if (orderQuantity >= 3 && orderQuantity <= 5) {
+    cookingTime = 15;
+  } else {
+    cookingTime = 20;
+  }
+  alert(
+    `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizzas will take ${cookingTime} minutes`
+  );
+} else {
+  alert(`Sorry, we don't have that pizza on the menu.`);
+}
